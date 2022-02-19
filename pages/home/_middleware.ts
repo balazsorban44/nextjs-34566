7710e3fad318957506673a4d2068c31bb1f8f9a9
Balázs Orbán <info@balazsorban.com>
2022-02-19 16:33:56 +0100

    Initial commit

---
+++ pages/home/_middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getBucket } from "@lib/ab-testing"
+import { HOME_BUCKETS } from "@lib/buckets"
+
+const COOKIE_NAME = "bucket-home"
+
+export function middleware(req: NextRequest) {
+  // Get the bucket cookie
+  const bucket = req.cookies[COOKIE_NAME] || getBucket(HOME_BUCKETS)
+
+  const destination = req.nextUrl.clone()
+  destination.pathname = `/home/${bucket}`
+  const res = NextResponse.rewrite(destination)
+
+  // Add the bucket to cookies if it's not there
+  if (!req.cookies[COOKIE_NAME]) {
+    res.cookie(COOKIE_NAME, bucket)
+  }
+
+  return res
+}
